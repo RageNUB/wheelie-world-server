ec68493141bae2d97e70c90238a0e915466a099a
RageNUB <jirimon39@gmail.com>
2023-05-19 04:36:58 +0600

    send all toy data to client from database

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dysamrx.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,6 +23,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("wheelieworld").collection("toycollection");
+
+    app.get("/products", async(req, res) => {
+        const result = await toyCollection.find().toArray();
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
