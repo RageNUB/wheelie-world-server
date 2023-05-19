70225aa57a276e4d35e2b9925bece9a9c62e8de6
RageNUB <jirimon39@gmail.com>
2023-05-19 15:44:40 +0600

    load single data from database and send to client

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,6 +29,13 @@ async function run() {
     app.get("/products", async(req, res) => {
         const result = await toyCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get("/products/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await toyCollection.findOne(query);
+      res.send(result);
     })
 
 
