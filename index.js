ac49b86cbc541820196203bec517a384ee642ab1
RageNUB <jirimon39@gmail.com>
2023-05-20 02:33:09 +0600

    delete method added

--- index.js
+++ index.js
@@ -55,6 +55,12 @@ async function run() {
       res.send(result);
     })
 
+    app.delete("/myToys/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
